77c63749a089b8c8b67296d5661f2ac5cfbb4504
sbose <sbose78@gmail.com>
2015-09-13 11:41:49 -0700

    modified table structures to include timestamp

--- sqls/common.sql
+++ sqls/common.sql
@@ -1,16 +1,15 @@
-CREATE TABLE IF NOT EXISTS `mobile_device_tokens` (
+CREATE TABLE `mobile_device_tokens` (
   `mobile_device_id` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `cloud_registration_id` longtext,
-  `user_id` longtext NOT NULL
+  `hardware_device_id` longtext NOT NULL,
+  `date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `mobile_device_type` longtext
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `mobile_device_tokens`
---
 
--- Test insert
 
-INSERT INTO `mobile_device_tokens` (`mobile_device_id`, `cloud_registration_id`, `user_id`) VALUES
+
+---INSERT INTO `mobile_device_tokens` (`mobile_device_id`, `cloud_registration_id`, `user_id`) VALUES
 ('4001', 'APA91bH_s8KxnQmUuDOjKkE1mNcV2f2Fx9bcZScvhHg06BjHAlDYRqN5hvwfdo0lFTNdlcE2U4XGhwzd1MKjxopiI8_DOlnSztWGXFaL3sNtNxa7pkHOTxYYsHmDKz07wrh6ZI8iPv5o', 'sbose78@gmail.com');
 
 
@@ -18,14 +17,14 @@ INSERT INTO `mobile_device_tokens` (`mobile_device_id`, `cloud_registration_id`,
 -- Table structure for table `task_status`
 --
 
-CREATE TABLE IF NOT EXISTS `task_status` (
+CREATE TABLE `task_status` (
   `task_id` int(11) NOT NULL AUTO_INCREMENT,
   `task_name` varchar(32) DEFAULT NULL,
   `input` varchar(5000) DEFAULT NULL,
   `output` varchar(256) DEFAULT NULL,
   `type` varchar(32) DEFAULT NULL,
   `celery_task_id` varchar(256) DEFAULT NULL,
+  `execution_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`task_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
+) ENGINE=InnoDB AUTO_INCREMENT=403 DEFAULT CHARSET=latin1;
 
